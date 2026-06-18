b2a9cee090c01b7b73ef4c39b0c07ccf8ef74571
Gabriel <105132671+gabrielcampeao@users.noreply.github.com>
2026-06-17 21:39:54 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import path from "path"
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portifolio/",
+  base: /",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
